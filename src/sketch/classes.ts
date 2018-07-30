c6ef9aeec93d5cc5610f53bcbd384028127f2fa6
Rupertofly <Rupertofly@users.noreply.github.com>
2018-07-30 13:30:27 +1000

    indenting yall

--- src/sketch/classes.ts
+++ src/sketch/classes.ts
@@ -9,158 +9,160 @@ type pt = [number, number];
  * @class VPoint
  */
 class VPoint {
-  public x: number;
-  public y: number;
-  public pgon: d3.VoronoiPolygon<VPoint>;
-  /**
-   * Creates an instance of VPoint.
-   * @param x
-   * @param y
-   * @param [pgon] optional create polygon
-   * @memberof VPoint
-   */
-  constructor(
-    x: number,
-    y: number,
-    pgon?: d3.VoronoiPolygon<VPoint>
-  ) {
-    this.x = x;
-    this.y = y;
-    this.pgon = pgon ? pgon : null;
-  }
-  /**
-   * Sets the position of this from the array
-   * @param array array to set from
-   * @memberof VPoint
-   */
-  public posFromArray = ( array: pt ) => {
-    [ this.x, this.y ] = array;
-  };
-  /**
-   * returns point in the form of an array
-   * @returns position
-   * @memberof VPoint
-   */
-  public posToArray = () =>
-    [ this.x, this.y ] as pt;
-  /**
-   * returns point in the form of a p5 vector
-   * @returns position
-   *
-   * @memberof VPoint
-   */
-  public posToVec = () =>
-    createVector( this.x, this.y );
-  /**
-   * sets the position of this point from a vector
-   * @param vec vector to set pos from
-   *
-   * @memberof VPoint
-   */
-  public posFromVec = ( vec: p5.Vector ) => {
-    [ this.x, this.y ] = vec.array();
-  };
-  /**
-   * returns the size pf the polygon as an array
-   * @returns size of polygon
-   *
-   * @memberof VPoint
-   */
-  public size = () => {
-    if ( !this.pgon ) {
-      throw new Error( 'no polygon registered' );
+    public x: number;
+    public y: number;
+    public pgon: d3.VoronoiPolygon<VPoint>;
+    /**
+     * Creates an instance of VPoint.
+     * @param x
+     * @param y
+     * @param [pgon] optional create polygon
+     * @memberof VPoint
+     */
+    constructor(
+        x: number,
+        y: number,
+        pgon?: d3.VoronoiPolygon<VPoint>
+    ) {
+        this.x = x;
+        this.y = y;
+        this.pgon = pgon ? pgon : null;
     }
-    return [
-      // x
-      Math.max.apply(
-        this,
-        this.pgon.map( p => p[0] )
-      ) -
-        Math.min.apply(
-          this,
-          this.pgon.map( p => p[0] )
-        ),
-      // y
-      Math.max.apply(
-        this,
-        this.pgon.map( p => p[1] )
-      ) -
-        Math.min.apply(
-          this,
-          this.pgon.map( p => p[1] )
-        )
-    ] as pt;
-  };
-  /**
-   * returns the polygons extent
-   * @returns extent of polygon
-   *
-   * @memberof VPoint
-   */
-  public extent = () => {};
-  /**
-   * returns the clipped polygon of this clipper
-   * @param clippingPolygon polygon to clip by
-   * @memberof VPoint
-   */
-  public clip = ( clippingPgon: pt[] ) => {
-    const clipped: d3.VoronoiPolygon<
-      VPoint
-    > = intersection(
-      this.pgon,
-      clippingPgon
-    )[0].slice(
-      0,
-      clippingPgon.length - 1
-    ) as d3.VoronoiPolygon<VPoint>;
-    clipped.data = this.pgon.data;
-    return clipped;
-  };
-  /**
-   * pad the polygon by an amount
-   * @param amount distance to pad
-   * @returns padded polygon
-   * @memberof VPoint
-   */
-  public pad = ( amount: number ) => {
-    const closedPoly = [
-      ...this.pgon,
-      this.pgon[0]
-    ];
-    const padded = new Offset()
-      .data( closedPoly )
-      .padding( amount )[0];
-    return padded.slice(
-      0,
-      padded.length - 2
-    ) as pt[];
-  };
+    /**
+     * Sets the position of this from the array
+     * @param array array to set from
+     * @memberof VPoint
+     */
+    public posFromArray = ( array: pt ) => {
+        [ this.x, this.y ] = array;
+    };
+    /**
+     * returns point in the form of an array
+     * @returns position
+     * @memberof VPoint
+     */
+    public posToArray = () =>
+        [ this.x, this.y ] as pt;
+    /**
+     * returns point in the form of a p5 vector
+     * @returns position
+     *
+     * @memberof VPoint
+     */
+    public posToVec = () =>
+        createVector( this.x, this.y );
+    /**
+     * sets the position of this point from a vector
+     * @param vec vector to set pos from
+     *
+     * @memberof VPoint
+     */
+    public posFromVec = ( vec: p5.Vector ) => {
+        [ this.x, this.y ] = vec.array();
+    };
+    /**
+     * returns the size pf the polygon as an array
+     * @returns size of polygon
+     *
+     * @memberof VPoint
+     */
+    public size = () => {
+        if ( !this.pgon ) {
+            throw new Error(
+                'no polygon registered'
+            );
+        }
+        return [
+            // x
+            Math.max.apply(
+                this,
+                this.pgon.map( p => p[0] )
+            ) -
+                Math.min.apply(
+                    this,
+                    this.pgon.map( p => p[0] )
+                ),
+            // y
+            Math.max.apply(
+                this,
+                this.pgon.map( p => p[1] )
+            ) -
+                Math.min.apply(
+                    this,
+                    this.pgon.map( p => p[1] )
+                )
+        ] as pt;
+    };
+    /**
+     * returns the polygons extent
+     * @returns extent of polygon
+     *
+     * @memberof VPoint
+     */
+    public extent = () => {};
+    /**
+     * returns the clipped polygon of this clipper
+     * @param clippingPolygon polygon to clip by
+     * @memberof VPoint
+     */
+    public clip = ( clippingPgon: pt[] ) => {
+        const clipped: d3.VoronoiPolygon<
+            VPoint
+        > = intersection(
+            this.pgon,
+            clippingPgon
+        )[0].slice(
+            0,
+            clippingPgon.length - 1
+        ) as d3.VoronoiPolygon<VPoint>;
+        clipped.data = this.pgon.data;
+        return clipped;
+    };
+    /**
+     * pad the polygon by an amount
+     * @param amount distance to pad
+     * @returns padded polygon
+     * @memberof VPoint
+     */
+    public pad = ( amount: number ) => {
+        const closedPoly = [
+            ...this.pgon,
+            this.pgon[0]
+        ];
+        const padded = new Offset()
+            .data( closedPoly )
+            .padding( amount )[0];
+        return padded.slice(
+            0,
+            padded.length - 2
+        ) as pt[];
+    };
 }
 class City {
-  public wards: Ward[];
-  public vorDiag: d3.VoronoiDiagram<Ward>;
-  private vorFunc: d3.VoronoiLayout<Ward>;
-
+    public wards: Ward[];
+    public vorDiag: d3.VoronoiDiagram<Ward>;
+    private vorFunc: d3.VoronoiLayout<Ward>;
 }
 
 class Ward extends VPoint {
-  public buildings: Building[];
-  private vorFunc: d3.VoronoiLayout<Building>;
-  constructor(
-    x: number,
-    y: number,
-    pgon?: d3.VoronoiPolygon<VPoint>
-  ) {
-    pgon ? super( x, y, pgon ) : super( x, y );
-  }
+    public buildings: Building[];
+
+    private vorFunc: d3.VoronoiLayout<Building>;
+    constructor(
+        x: number,
+        y: number,
+        pgon?: d3.VoronoiPolygon<VPoint>
+    ) {
+        pgon ? super( x, y, pgon ) : super( x, y );
+    }
 }
 
 class Building extends VPoint {
-  constructor(
-    x: number,
-    y: number,
-    pgon?: d3.VoronoiPolygon<VPoint>
-  ) {
-    pgon ? super( x, y, pgon ) : super( x, y );
-  }
+    constructor(
+        x: number,
+        y: number,
+        pgon?: d3.VoronoiPolygon<VPoint>
+    ) {
+        pgon ? super( x, y, pgon ) : super( x, y );
+    }
 }
--- src/sketch/main.ts
+++ src/sketch/main.ts
@@ -6,79 +6,79 @@ import pad from 'polygon-offset';
 import * as hf from './lib/helperFuncs';
 type pt = [number, number];
 const polyPoints = [
-  [ 108.5, 41 ],
-  [ 55.5, 375 ],
-  [ 484.5, 427 ],
-  [ 798.5, 408 ],
-  [ 808.5, 166 ]
+    [ 108.5, 41 ],
+    [ 55.5, 375 ],
+    [ 484.5, 427 ],
+    [ 798.5, 408 ],
+    [ 808.5, 166 ]
 ] as pt[];
 const clip = [
-  [ 108.5, 241 ],
-  [ 65.5, 375 ],
-  [ 484.5, 527 ],
-  [ 698.5, 408 ],
-  [ 808.5, 166 ],
-  [ 654.5, 231 ]
+    [ 108.5, 241 ],
+    [ 65.5, 375 ],
+    [ 484.5, 527 ],
+    [ 698.5, 408 ],
+    [ 808.5, 166 ],
+    [ 654.5, 231 ]
 ] as pt[];
 let padding = 5;
 export function setup() {
-  createCanvas( 860, 480 ).parent( '#canvas' );
-  background( 55 );
+    createCanvas( 860, 480 ).parent( '#canvas' );
+    background( 55 );
 }
 
 export function draw() {
-  background( 55 );
-  // clip
-  noFill();
-  strokeWeight( 3 );
-  stroke( 255, 0, 0, 150 );
-  beginShape();
-  clip.map( p => vertex.apply( this, p ) );
-  endShape( CLOSE );
+    background( 55 );
+    // clip
+    noFill();
+    strokeWeight( 3 );
+    stroke( 255, 0, 0, 150 );
+    beginShape();
+    clip.map( p => vertex.apply( this, p ) );
+    endShape( CLOSE );
 
-  // og shape
-  fill( 0, 255, 0, 50 );
-  strokeWeight( 3 );
-  stroke( 0, 255, 0, 150 );
-  beginShape();
-  polyPoints.map( p => vertex.apply( this, p ) );
-  endShape( CLOSE );
-  const z = greinerHormann;
-  // clipped shape
-  let clipped: any = greinerHormann.intersection(
-    polyPoints,
-    clip
-  );
-  clipped = clipped[0];
-  fill( 0, 0, 255, 50 );
-  strokeWeight( 3 );
-  stroke( 0, 0, 255, 150 );
-  beginShape();
-  clipped.map( p => vertex.apply( this, p ) );
-  endShape();
-  // padded
-  const padded = new pad()
-    .data( clipped )
-    .padding( padding );
-  fill( 255, 255, 255, 50 );
-  strokeWeight( 3 );
-  stroke( 255, 255, 255, 150 );
-  beginShape();
-  padded[0].map( p => vertex.apply( this, p ) );
-  endShape();
+    // og shape
+    fill( 0, 255, 0, 50 );
+    strokeWeight( 3 );
+    stroke( 0, 255, 0, 150 );
+    beginShape();
+    polyPoints.map( p => vertex.apply( this, p ) );
+    endShape( CLOSE );
+    const z = greinerHormann;
+    // clipped shape
+    let clipped: any = greinerHormann.intersection(
+        polyPoints,
+        clip
+    );
+    clipped = clipped[0];
+    fill( 0, 0, 255, 50 );
+    strokeWeight( 3 );
+    stroke( 0, 0, 255, 150 );
+    beginShape();
+    clipped.map( p => vertex.apply( this, p ) );
+    endShape();
+    // padded
+    const padded = new pad()
+        .data( clipped )
+        .padding( padding );
+    fill( 255, 255, 255, 50 );
+    strokeWeight( 3 );
+    stroke( 255, 255, 255, 150 );
+    beginShape();
+    padded[0].map( p => vertex.apply( this, p ) );
+    endShape();
 }
 
 export function mousePressed() {}
 export function keyReleased() {
-  switch ( key ) {
-    case 'A':
-      padding -= padding > 1 ? 1 : 0;
-      console.log( padding );
-      break;
-    case 'D':
-      padding += padding < 15 ? 1 : 0;
-      console.log( padding );
-      
-      break;
-  }
+    switch ( key ) {
+        case 'A':
+            padding -= padding > 1 ? 1 : 0;
+            console.log( padding );
+            break;
+        case 'D':
+            padding += padding < 15 ? 1 : 0;
+            console.log( padding );
+
+            break;
+    }
 }
